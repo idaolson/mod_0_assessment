4339fe78926f11c346e8b58b5fed1b4d67c4c9d5
Ida Olson <idaolson@gmail.com>
2021-05-20 18:01:47 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+anmial = "cactus"
+is_cuddly = false
+anmial = "Chibi"
+
+num_8 = 2
+num_3 = 4.6
+quotient = num_3 / num_8
+
+favorite_films = ['Knives Out','Captain Marvel','Arrival']
+favorite_films.shift
+
+puts anmial.length
