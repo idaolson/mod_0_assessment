b2762fb57713863bbb0081e0104d92009efa879f
Ida Olson <idaolson@gmail.com>
2021-05-20 18:28:13 -0600

    Add house hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house = {
+  "roof" => "asphalt shingle",
+  "siding" => "vinyl",
+  "square_feet" => 920
+  "bathrooms" => 1.5
+  "is_for_rent" => false,
+  "is_for_sale" => false,
+  "levels" => ['main', 'attic', 'basement', 'crawlspace'],
+  "flooring types" => ['carpet', 'tile', 'vinyl']
+}
