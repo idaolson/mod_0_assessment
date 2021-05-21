59503e8b763163f9df43bd08c75cb0e073a0ea16
Ida Olson <idaolson@gmail.com>
2021-05-20 18:21:12 -0600

    Add laptop hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop = {
+  "platform" => "mac",
+  "color" => "space gray",
+  "os_version" => 11.3,
+  "size_in_inches" => 13,
+  "is_new" => true,
+  "is_shiny" => true,
+  "applications" => ['atom', 'imovie', 'rescuetime'],
+  "settings" => ['volume', "brightness", "battery power"]
+}
